8cb7485ee50e7a02ee1ddea98416bb5ee37963c5
Carlos Jesus Perez Dugarte <carlos.perez@vicentelopez.gov.ar>
2022-02-14 14:33:01 -0300

    Se configuro el servidor y el puerto

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('my-app');
+const express = require('express');
+const app = express();
+const port = 3100;
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo');
+});
+
+app.listen(port, () => {
+  console.log('port active ' + port);
+})
